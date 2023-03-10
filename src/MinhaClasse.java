5cd002e44826834ea65362b91bc8014ea8fab019
mokshano11 <mateusggruber@gmail.com>
2023-03-10 00:04:07 -0500

    declarando metodos

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,7 +1,17 @@
 public class MinhaClasse {
 
-    public static void main (String [] args) {
+    public static void main (String [] args) {  /* metodo "main" */
 
+        String primeiroNome = "Mateus"; /*variavel 1 */
+        String segundoNome = "Gruber"; /*variavel 2 */
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome); /*variavel chamando um metodo */
+
+        System.out.print (nomeCompleto); /*saida do programa */
+     }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {   /* metodo */
+        return "Resultado Do Metodo " + primeiroNome.concat(" ").concat(segundoNome);  /*o que o metodo esta retornando */
     }
 
 }
