cb318c9bd54a2572c16212d682f916dc52a17794
mokshano11 <mateusggruber@gmail.com>
2023-03-09 23:13:21 -0500

    apagando exercicio anterior

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -2,13 +2,6 @@ public class MinhaClasse {
 
     public static void main (String [] args) {
 
-        int ano = 2021;
-
-        ano = 2022;
-
-        final String BR = "Brasil";
-
-        BR = "Brazil";
     }
 
 }
