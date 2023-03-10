adda648853368ff5ecf02f83cf0e83c90db647cb
mokshano11 <mateusggruber@gmail.com>
2023-03-09 23:42:28 -0500

    declarando variaveis e metodos 1

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,8 +1,16 @@
 public class MinhaClasse {
 
     public static void main (String [] args) {
+        String meuNome = "Mateus"; /*variavel de caracteres */
 
-        
+        int anoFabricacao = 2023; /* vairavel de numeros inteiros */
+
+        boolean verdadeira = true; /*variavel com valor logico, nao numerico */
+
+        anoFabricacao = 2024; /*alterando o valor da variavel */
+
+        /* todas as variaveis serao declaradas com TIPO - NOME - ATRIBUICAO correspondente com a tipagem especifica */
+
     }
 
 }
